1b93b3474f883bdbf02565607ed45545001ab04a
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-04-19 14:48:42 -0300

    js

--- EA2/ejemploJavaScript3.js
+++ EA2/ejemploJavaScript3.js
@@ -14,3 +14,56 @@ function sumar()
     alert(mensaje);
     document.getElementById("lblMensaje").innerHTML = mensaje;
 }
+function restar()
+{
+    let n1 = document.getElementById("numero1").value;
+    let n2 = document.getElementById("numero2").value;
+
+    if(n1 == "" || n2 == "")
+    {
+        alert("No especifico un número");
+        return;
+    }
+
+    let total = parseInt(n1) - parseInt(n2);
+    let mensaje = "El total es: " + total; 
+    alert(mensaje);
+    document.getElementById("lblMensaje").innerHTML = mensaje;
+}
+function multiplicar()
+{
+    let n1 = document.getElementById("numero1").value;
+    let n2 = document.getElementById("numero2").value;
+
+    if(n1 == "" || n2 == "")
+    {
+        alert("No especifico un número");
+        return;
+    }
+
+    let total = parseInt(n1) * parseInt(n2);
+    let mensaje = "El total es: " + total; 
+    alert(mensaje);
+    document.getElementById("lblMensaje").innerHTML = mensaje;
+}
+function dividir()
+{
+    let n1 = document.getElementById("numero1").value;
+    let n2 = document.getElementById("numero2").value;
+
+    if(n1 == "" || n2 == "")
+    {
+        alert("No especifico un número");
+        return;
+    }
+    n2 = parseInt(n2);
+    if(n2 == 0)
+    {
+        alert("No se puede dividir por cero");
+        return; 
+    }
+    let total = parseInt(n1) / parseInt(n2);
+    let mensaje = "El total es: " + total; 
+    alert(mensaje);
+    document.getElementById("lblMensaje").innerHTML = mensaje;
+}
