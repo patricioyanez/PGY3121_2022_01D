1ac3340c9a746c175d10f68cd556fca03f2317b1
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-05-23 10:27:22 -0400

    creacion plantilla y MTV

--- EA3/proyecto1/proyecto1/crud/admin.py
+++ EA3/proyecto1/proyecto1/crud/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Marca
+from .models import Marca, Categoria
 
 class MarcaAdmin(admin.ModelAdmin):
     list_display = ['nombre', 'activo']
     list_filter  = ['nombre']
 
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'activo']
+    list_filter  = ['nombre', 'activo']
 
 # Register your models here.
 admin.site.register(Marca, MarcaAdmin)
+admin.site.register(Categoria, CategoriaAdmin)
--- EA3/proyecto1/proyecto1/crud/models.py
+++ EA3/proyecto1/proyecto1/crud/models.py
@@ -7,3 +7,10 @@ class Marca(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Categoria(models.Model):
+    nombre = models.TextField(max_length=100)
+    activo = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
