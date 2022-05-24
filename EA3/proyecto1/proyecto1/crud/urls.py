d44930a537677e88e25a11285da192c431dee2c2
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-05-24 12:48:59 -0400

    plantillas herencia y block

--- EA3/proyecto1/proyecto1/crud/urls.py
+++ EA3/proyecto1/proyecto1/crud/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('categoria', views.categoria , name='categoria'),
+    path('crudMarca', views.marca , name='marca'),
     ]
+    # crud => esta definido en el urls del proyecto base (proyecto1 urls.py)
+    # 127.0.0.1:8000/crud/categoria
--- EA3/proyecto1/proyecto1/crud/views.py
+++ EA3/proyecto1/proyecto1/crud/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def categoria(request):
-    return render(request, 'categoria.html', {'nombre': ''})
+    return render(request, 'categoria.html', {'nombre': 'Ana'})
+
+def marca(request):
+    return render(request, 'marca.html', {})
+
 
