ab500d66422774d5cdefe2cf4aac12fbad7a1669
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-05-26 16:40:09 -0400

    form, POST

--- EA3/proyecto1/proyecto1/crud/views.py
+++ EA3/proyecto1/proyecto1/crud/views.py
@@ -2,7 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 def categoria(request):
-    return render(request, 'categoria.html', {'nombre': 'Ana'})
+    contexto = {}
+    if request.method == 'POST':
+        id      = request.POST["txtId"]
+        nombre  = request.POST["txtNombre"]
+        #activo  = request.POST["chkActivo"]
+        
+        if 'btnGuardar' in request.POST:
+            # orm ??? django
+            contexto = {'btn': 'guardar'}
+        elif 'btnListar' in request.POST:
+            contexto = {'btn': 'btnListar'}
+        elif 'btnEliminar' in request.POST:
+            contexto = {'btn': 'btnEliminar'}
+        elif 'btnListar' in request.POST:
+            contexto = {'btn': 'btnListar'}
+
+    return render(request, 'categoria.html', contexto)
 
 def marca(request):
 
